b25b00b82cdeed3c49338cbb1d48cf2cc6be3b06
HaThanhTuan <109499178+tuan04@users.noreply.github.com>
2026-08-22 00:11:08 +0700

    config: websocket in notification servicce

--- core-service/resource-management-service/src/main/java/iuh/fit/resourcemanagementservice/entity/Campaign.java
+++ core-service/resource-management-service/src/main/java/iuh/fit/resourcemanagementservice/entity/Campaign.java
@@ -2,7 +2,6 @@ package iuh.fit.resourcemanagementservice.entity;
 
 import iuh.fit.resourcemanagementservice.enums.CampaignStatus;
 import jakarta.persistence.*;
-import jdk.jfr.Enabled;
 import lombok.*;
 
 import java.time.LocalDate;

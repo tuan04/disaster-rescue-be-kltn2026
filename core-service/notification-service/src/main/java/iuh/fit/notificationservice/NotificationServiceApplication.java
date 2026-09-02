8ef627e94f5bbe7b47cc2b01628c550a964fc4ae
HaThanhTuan <109499178+tuan04@users.noreply.github.com>
2026-09-02 15:33:03 +0700

    config: cấu hình kafka trong file yml

--- core-service/notification-service/src/main/java/iuh/fit/notificationservice/NotificationServiceApplication.java
+++ core-service/notification-service/src/main/java/iuh/fit/notificationservice/NotificationServiceApplication.java
@@ -2,8 +2,10 @@ package iuh.fit.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication(scanBasePackages = {"iuh.fit.notificationservice", "iuh.fit.common"})
+@EnableKafka
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
--- core-service/notification-service/src/main/java/iuh/fit/notificationservice/config/WebSocketConfig.java
+++ core-service/notification-service/src/main/java/iuh/fit/notificationservice/config/WebSocketConfig.java
@@ -12,23 +12,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Kích hoạt In-Memory Broker cho /topic (kênh broadcast) và /queue (kênh riêng)
         registry.enableSimpleBroker("/topic", "/queue");
 
-        // Prefix cho các message gửi từ client lên controller (@MessageMapping)
         registry.setApplicationDestinationPrefixes("/app");
 
-        // Prefix cho các kênh gửi riêng đến từng user cụ thể
         registry.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // Endpoint WebSocket tiêu chuẩn cho Mobile & Web STOMP Client
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*");
 
-        // Endpoint WebSocket hỗ trợ SockJS fallback
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
